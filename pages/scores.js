18cbaeabeb27ed4637a6d6428424b53799c0aefb
Alex <alexpeter@northwesternmutual.com>
2021-10-27 08:16:44 -0500

    feat: scores page + misc

---
+++ pages/scores.js
@@ -0,0 +1,37 @@
+import styles from '../styles/Home.module.css'
+
+function scores ({ scores }) {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Top {scores.length} Scores</h1>
+        <table className={styles.table}>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+          </tr>
+          {scores.map((score, i) => {
+            return (
+              <tr key={i}>
+                <td className={styles.td}>{score.name}</td>
+                <td className={styles.td}>{score.score}</td>
+              </tr>
+            )
+          })}
+        </table>
+      </main>
+    </div>
+  )
+}
+
+export async function getStaticProps () {
+  const res = await fetch('https://ms-diamant.herokuapp.com/user')
+  const scores = await res.json()
+  return {
+    props: {
+      scores
+    }
+  }
+}
+
+export default scores
