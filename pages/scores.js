b98df11f8d20d34535d89f5b21e02fedb057c9d2
Alex <alexpeter@northwesternmutual.com>
2021-11-09 18:59:05 -0600

    fix: adds logging

--- pages/scores.js
+++ pages/scores.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 
 function scores ({ scores }) {
+  console.log('scores:', scores)
   return (
     <div className={styles.container}>
       <main className={styles.main}>
