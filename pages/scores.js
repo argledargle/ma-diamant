033aceee465df9e951a6bd54c39b85f8c4d240b0
Alex <alexpeter@northwesternmutual.com>
2021-11-09 20:11:10 -0600

    fix: getScores function

--- pages/scores.js
+++ pages/scores.js
@@ -1,40 +1,56 @@
+import { set } from 'lodash'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-function scores ({ scores }) {
-  console.log('scores:', scores)
+function Scores () {
+  const [state, setState] = useState('loading')
+  const [scores, setScores] = useState([])
+
+  useEffect(async () => {
+    try {
+      async function getScores () {
+        const res = await fetch('https://ms-diamant.herokuapp.com/user')
+        const scores = await res.json()
+        return scores
+      }
+      const scores = await getScores()
+      setScores(scores)
+      setState('success')
+    } catch (err) {
+      console.error('error:', error)
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Top {scores.length} Scores</h1>
-        <table className={styles.table}>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th>Score</th>
-            </tr>
-            {scores.map((score, i) => {
-              return (
-                <tr key={i}>
-                  <td className={styles.td}>{score.name}</td>
-                  <td className={styles.td}>{score.score}</td>
+        {state === 'loading' ? (
+          <p>Loading</p>
+        ) : (
+          <>
+            <h1>Top {scores.length} Scores</h1>
+            <table className={styles.table}>
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <th>Score</th>
                 </tr>
-              )
-            })}
-          </tbody>
-        </table>
+                {scores.map((score, i) => {
+                  return (
+                    <tr key={i}>
+                      <td className={styles.td}>{score.name}</td>
+                      <td className={styles.td}>{score.score}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </>
+        )}
       </main>
     </div>
   )
 }
 
-export async function getStaticProps () {
-  const res = await fetch('https://ms-diamant.herokuapp.com/user')
-  const scores = await res.json()
-  return {
-    props: {
-      scores
-    }
-  }
-}
 
-export default scores
+export default Scores
